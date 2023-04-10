123e15476caaeed4ab8fcabc8d4b84cf3823241e
Daniel Ekman <knegge@gmail.com>
2023-04-10 15:42:22 +0200

    Update satyaml live updater to launch next program
    Signed-off-by: Daniel Ekman <knegge@gmail.com>

--- addons/liveupdate-satyaml.sh
+++ addons/liveupdate-satyaml.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd
+cd || exit
 git clone -b maint-3.8 --depth 1 https://github.com/daniestevez/gr-satellites.git
 cp gr-satellites/python/satyaml/* /usr/lib/python3/dist-packages/satellites/satyaml/
 rm -rf gr-satellites /tmp/.satnogs/grsat_list.*
+exec "$@"
 
