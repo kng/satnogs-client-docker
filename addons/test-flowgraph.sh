69d39a2712b3d10f8dcfb2ae87cececbd52b2550
Daniel Ekman <knegge@gmail.com>
2023-04-10 12:05:52 +0200

    Add base image as ARG, cleanup
    Signed-off-by: Daniel Ekman <knegge@gmail.com>

--- addons/test-flowgraph.sh
+++ addons/test-flowgraph.sh
@@ -13,10 +13,19 @@ if [ -z "${SATNOGS_PPM_ERROR}" ]; then
 fi
 
 BIN="satnogs_fm.py"
-ARGS="--soapy-rx-device=${SATNOGS_SOAPY_RX_DEVICE} --samp-rate-rx=${SATNOGS_RX_SAMP_RATE} --rx-freq=${RX_FREQ} --file-path=test.ogg --waterfall-file-path=test.dat --decoded-data-file-path=test_data --gain=${SATNOGS_RF_GAIN} --antenna=${SATNOGS_ANTENNA} --ppm=${SATNOGS_PPM_ERROR}"
+ARGS="--soapy-rx-device=${SATNOGS_SOAPY_RX_DEVICE} \
+      --samp-rate-rx=${SATNOGS_RX_SAMP_RATE} \
+      --rx-freq=${RX_FREQ} \
+      --file-path=test.ogg \
+      --waterfall-file-path=test.dat \
+      --decoded-data-file-path=test_data \
+      --gain=${SATNOGS_RF_GAIN} \
+      --antenna=${SATNOGS_ANTENNA} \
+      --ppm=${SATNOGS_PPM_ERROR} \
+      --rigctl-host=${SATNOGS_RIG_IP} \
+      --rigctl-port=${SATNOGS_RIG_PORT}"
 
 echo "Running rx at ${RX_FREQ}, press Ctrl-C to terminate."
-rigctl -m 2 -r 127.0.0.1:4532 F "${RX_FREQ}"
 ${BIN} ${ARGS}
 echo "Generating waterfall..."
 wf2png.py test
