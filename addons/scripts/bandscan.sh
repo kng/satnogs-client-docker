08fbcaa66481d85fe8c0cfde6fd2bf37e57c36ec
Daniel Ekman <knegge@gmail.com>
2023-08-19 15:55:15 +0200

    Update bandscan, better defaults, added settings
    Signed-off-by: Daniel Ekman <knegge@gmail.com>

--- addons/scripts/bandscan.sh
+++ addons/scripts/bandscan.sh
@@ -1,35 +1,52 @@
 #!/bin/bash
-
+# exit if pipeline fails or unset variables
+set -eu
 # default values
-if [ -z "$BANDSCAN_FREQ" ]; then
-    BANDSCAN_FREQ=401000000
-fi
-if [ -z "$BANDSCAN_SAMPLERATE" ]; then
-    BANDSCAN_SAMPLERATE="$SATNOGS_RX_SAMP_RATE"
-fi
-if [ -z "$BANDSCAN_DIR" ]; then
-    BANDSCAN_DIR=/srv/bandscan
-fi
-if [ -z "$SATNOGS_PPM_ERROR" ]; then
-    SATNOGS_PPM_ERROR=0
-fi
-if [ -z "$SATNOGS_RF_GAIN" ]; then
-    SATNOGS_RF_GAIN=0
-fi
+: "${BANDSCAN_FREQ?Needs to be defined.}"
+: "${BANDSCAN_SAMPLERATE:=$SATNOGS_RX_SAMP_RATE}"
+: "${BANDSCAN_DIR:=/srv/bandscan}"
+: "${SATNOGS_PPM_ERROR:=0}"
+: "${BANDSCAN_PID:=/tmp/.satnogs/bandscan.pid}"
+: "${BANDSCAN_BIN:=rx_sdr}"
+: "${BANDSCAN_OUTPUT_FORMAT:=CF32}"
+: "${BANDSCAN_INPUT_FORMAT:=float}"
+: "${SATNOGS_RF_GAIN:=0}"
+: "${SATNOGS_OTHER_SETTINGS:=0}"
 
-BANDSCAN_PID="/tmp/.satnogs/bandscan.pid"
-BANDSCAN_BIN="rx_sdr"
-OUTPUT_FORMAT="CF32"
-INPUT_FORMAT="float"
+# if unset, try calculating channels
+if [ -n "${BANDSCAN_CHANNELS:-}" ]; then
+  CHANNELS="$BANDSCAN_CHANNELS"
+elif [ "${BANDSCAN_FREQ%.*}" -lt 300000000 ]; then
+  CHANNELS="20" # VHF
+elif [ "${BANDSCAN_FREQ%.*}" -lt 500000000 ]; then
+  CHANNELS="50" # UHF
+else
+  CHANNELS="100" # S-BAND
+fi
 
 if [ "${1^^}" == "START" ] && [ "${BANDSCAN_ENABLE^^}" == "YES" ]; then
-    echo "Starting bandscan"
+    echo "Starting bandscan at $BANDSCAN_FREQ"
     DAY=$(date -Idate)
     SAVEDIR="$BANDSCAN_DIR/$BANDSCAN_FREQ/$DAY"
     mkdir -p "$SAVEDIR" "$(dirname "$BANDSCAN_PID")"
     INDEX=$(find "$SAVEDIR" -mindepth 1 -maxdepth 1 -type f | wc -l)
-    $BANDSCAN_BIN -d "$SATNOGS_SOAPY_RX_DEVICE" -a "$SATNOGS_ANTENNA" -p "$SATNOGS_PPM_ERROR" -g "$SATNOGS_RF_GAIN" -s "$BANDSCAN_SAMPLERATE" -f "$BANDSCAN_FREQ" -F "$OUTPUT_FORMAT" - \
-    | rffft -q -f "$BANDSCAN_FREQ" -s "$BANDSCAN_SAMPLERATE" -F "$INPUT_FORMAT" -c 50 -t 1 -p "$SAVEDIR" -o "$DAY" -S "$INDEX" &
+    $BANDSCAN_BIN -d "$SATNOGS_SOAPY_RX_DEVICE" \
+                  -a "$SATNOGS_ANTENNA" \
+                  -p "$SATNOGS_PPM_ERROR" \
+                  -g "$SATNOGS_RF_GAIN" \
+                  -t "$SATNOGS_OTHER_SETTINGS" \
+                  -s "$BANDSCAN_SAMPLERATE" \
+                  -f "$BANDSCAN_FREQ" \
+                  -F "$BANDSCAN_OUTPUT_FORMAT" - \
+    | rffft -q \
+            -f "$BANDSCAN_FREQ" \
+            -s "$BANDSCAN_SAMPLERATE" \
+            -F "$BANDSCAN_INPUT_FORMAT" \
+            -c "$CHANNELS" \
+            -t 1 \
+            -p "$SAVEDIR" \
+            -o "$DAY" \
+            -S "$INDEX" &
     echo $! > "$BANDSCAN_PID"
 fi
 
@@ -40,4 +57,3 @@ if [ "${1^^}" == "STOP" ]; then
        rm -f "$BANDSCAN_PID"
    fi
 fi
-
