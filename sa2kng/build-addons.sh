668dadf7ddba289955b027ed84eac3079954cb12
Daniel Ekman <knegge@gmail.com>
2023-09-08 12:32:51 +0200

    Add repo root, optional git rtl-sdr
    Signed-off-by: Daniel Ekman <knegge@gmail.com>

--- sa2kng/build-addons.sh
+++ sa2kng/build-addons.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 export DOCKER_BUILDKIT=1
 TAG="sa2kng-addons"
+REPO_ROOT="knegge"
 SATNOGS_IMAGE_TAG="sa2kng"
-ARGS="--build-arg SATNOGS_IMAGE_TAG=${SATNOGS_IMAGE_TAG}"
+ARGS="
+    --build-arg SATNOGS_IMAGE_TAG=${SATNOGS_IMAGE_TAG} \
+    --build-arg REPO_ROOT=${REPO_ROOT} \
+"
 
-docker build -t librespace/satnogs-client:${TAG} ${ARGS} ../addons
+docker build -t ${REPO_ROOT}/satnogs-client:${TAG} ${ARGS} ../addons
--- sa2kng/build.sh
+++ sa2kng/build.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 export DOCKER_BUILDKIT=1
 TAG="sa2kng"
-ARGS="--build-arg GRSATNOGS_URL=https://gitlab.com/knegge/gr-satnogs.git \
-      --build-arg GRSATNOGS_BRANCH=master \
-      --build-arg GRSOAPY_URL=https://gitlab.com/knegge/gr-soapy.git \
-      --build-arg GRSOAPY_BRANCH=master \
-      --build-arg FLOWGRAPHS_URL=https://gitlab.com/knegge/satnogs-flowgraphs.git \
-      --build-arg FLOWGRAPHS_BRANCH=sa2kng \
-      --build-arg CLIENT_URL=https://gitlab.com/knegge/satnogs-client.git \
-      --build-arg CLIENT_BRANCH=sa2kng"
+REPO_ROOT="knegge"
+ARGS="
+    --build-arg GRSATNOGS_URL=https://gitlab.com/knegge/gr-satnogs.git \
+    --build-arg GRSATNOGS_BRANCH=${TAG} \
+    --build-arg GRSATNOGS_VER=2.3.4.0-${TAG} \
+    --build-arg GRSOAPY_URL=https://gitlab.com/knegge/gr-soapy.git \
+    --build-arg GRSOAPY_BRANCH=${TAG} \
+    --build-arg GRSOAPY_VER=2.1.3.1-${TAG} \
+    --build-arg FLOWGRAPHS_URL=https://gitlab.com/knegge/satnogs-flowgraphs.git \
+    --build-arg FLOWGRAPHS_BRANCH=${TAG} \
+    --build-arg FLOWGRAPHS_VER=1.5-${TAG} \
+    --build-arg CLIENT_URL=https://gitlab.com/knegge/satnogs-client.git \
+    --build-arg CLIENT_BRANCH=${TAG} \
+    --build-arg RTLSDR_URL=https://github.com/rtlsdrblog/rtl-sdr-blog.git \
+    --build-arg RTLSDR_BRANCH=master \
+"
 
-docker build -t librespace/satnogs-client:${TAG} ${ARGS} .
+docker build -t ${REPO_ROOT}/satnogs-client:${TAG} ${ARGS} .
