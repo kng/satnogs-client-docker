e34be75b37414b4b6299df46b09c4b46a115e85f
Daniel Ekman <knegge@gmail.com>
2024-01-02 12:43:55 +0100

    single quotes, default timestamp, cas5a initial
    Signed-off-by: Daniel Ekman <knegge@gmail.com>

--- addons/scripts/imagedecode.py
+++ addons/scripts/imagedecode.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 import logging
+from datetime import datetime
 from io import BytesIO
 from os import path, getenv
 from pathlib import Path
 from subprocess import Popen, DEVNULL
 from sys import argv
 
-logging.basicConfig(format="%(name)s - %(levelname)s - %(message)s",
-                    level=getattr(logging, getenv("SATNOGS_LOG_LEVEL", "WARNING")))
-LOGGER = logging.getLogger("imagedecode")
+logging.basicConfig(
+    format='%(name)s - %(levelname)s - %(message)s',
+    level=getattr(logging, getenv('SATNOGS_LOG_LEVEL', 'WARNING')),
+)
+LOGGER = logging.getLogger('imagedecode')
 
 
 class ImageDecode(object):
@@ -19,8 +22,10 @@ class ImageDecode(object):
         self.imagedata = BytesIO()
         if image_file is None:  # default to the same basename
             self.image_name = path.splitext(frame_file)[0]
+            self.image_ts = ''
         else:
             self.image_name = image_file
+            self.image_ts = datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
         self.image_ext = '.jpg'
         if self.norad_id > 0:
             self.auto_decode()
@@ -36,23 +41,23 @@ class ImageDecode(object):
                              57167]:  # StratoSat TK-1
             self.stratosat_parse_frames()
         else:
-            LOGGER.debug(f"No image decoder found for {self.norad_id}")
+            LOGGER.debug(f'No image decoder found for {self.norad_id}')
             return
         self.write_image()
 
     def parse_file(self):
         try:
-            with open(self.frame_file, "r") as f:
+            with open(self.frame_file, 'r') as f:
                 return self.parse_hex_file(f)
         except UnicodeDecodeError:
-            with open(self.frame_file, "rb") as f:
+            with open(self.frame_file, 'rb') as f:
                 return self.parse_kiss_file(f)
 
     def parse_hex_file(self, hex_file):
         for row in hex_file:
-            row = row.replace(" ", "").strip()
-            if "|" in row:
-                row = row.split("|")[-1]
+            row = row.replace(' ', '').strip()
+            if '|' in row:
+                row = row.split('|')[-1]
                 self.frames.append(row)
 
     def parse_kiss_file(self, kiss_file):
@@ -73,12 +78,12 @@ class ImageDecode(object):
         for row in self.frames:  # find start frame, memory offset, detect lr/hr
             if len(row) != 128:
                 continue
-            if row[16:22].upper() == "FFD8FF":
+            if row[16:22].upper() == 'FFD8FF':
                 offset = int((row[12:14] + row[10:12]), 16)
-                hr = row[6:10].upper() == "2098"
+                hr = row[6:10].upper() == '2098'
                 cmd_match = row[0:4].upper()
                 break
-        LOGGER.debug(f'Stratosat parser: cmd_match={cmd_match}, offset={offset}, hr={hr}')
+        LOGGER.debug(f'Stratosat: cmd_match={cmd_match}, offset={offset}, hr={hr}')
         for row in self.frames:
             if len(row) != 128:
                 continue
@@ -93,29 +98,73 @@ class ImageDecode(object):
                 self.imagedata.seek(addr)
                 self.imagedata.write(bytes.fromhex(payload))
 
+    def cas5a_process_images(self):
+        num_images = 0
+        for pid in set(self.cas5a_get_photo_ids()):
+            self.cas5a_parse_frames(pid)
+            self.image_ext = f'_{num_images}.jpg'
+            self.write_image()
+            num_images += 1
+
+    def cas5a_parse_frames(self, pid):
+        dlen = 240  # assumed maxed out frames to multiply by sequence number
+        hlen = 32  # header length
+        for row in self.frames:
+            if len(row) < 32:
+                continue
+            if pid != row[46:64] or int(row[32:34], 16) != 3:
+                continue
+            ftot = int(row[34:38], 16)
+            fseq = int(row[38:42], 16)
+            flen = int(row[42:46], 16) * 2 + hlen
+            if fseq <= ftot and flen <= len(row):
+                self.imagedata.seek((fseq - 1) * dlen)
+                self.imagedata.write(bytes.fromhex(row[64:flen]))
+
+    def cas5a_get_photo_ids(self):
+        pids = set()
+        for row in self.frames:
+            if len(row) < 32:
+                continue
+            if (
+                int(row[32:34], 16) != 3
+                or int(row[46:48], 16) < 22  # year
+                or int(row[46:48], 16) > 25  # year
+                or int(row[48:50], 16) > 12  # month
+                or int(row[50:52], 16) > 31  # day
+                or int(row[52:54], 16) > 24  # hour
+                or int(row[54:56], 16) > 60  # minute
+                or int(row[56:58], 16) > 60  # second
+            ):  # sanity check
+                continue
+            pids.add(row[46:64])
+        return pids
+
     def write_image(self):
         if self.imagedata.getbuffer().nbytes == 0:
             LOGGER.warning(f'No image data found in {self.frame_file}')
             return
-        image_file = f'{self.image_name}{self.image_ext}'
+        image_file = f'{self.image_name}{self.image_ts}{self.image_ext}'
         LOGGER.info(f'Writing image to: {image_file}')
-        with open(image_file, "wb") as f:
+        with open(image_file, 'wb') as f:
             f.write(self.imagedata.getbuffer())
 
     def jy1sat_ssdv(self):
-        LOGGER.info(f"Running jy1sat_ssdv for {self.norad_id}")
+        LOGGER.info(f'Running jy1sat_ssdv for {self.norad_id}')
         Popen(
-            ["jy1sat_ssdv", self.frame_file, self.image_name],
+            ['jy1sat_ssdv', self.frame_file, self.image_name],
             stdout=DEVNULL,
             stderr=DEVNULL,
         )
-        for f in Path(self.image_name).glob("*.ssdv"):
+        for f in Path(self.image_name).glob('*.ssdv'):
             f.unlink(missing_ok=True)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(argv) != 3:
-        print(f'Usage: {argv[0]} <frame_file> <norad_id>\n'
-              f'Frame file can be KISS, SatNOGS data export or GetKISS+')
+        print(
+            f'Usage: {argv[0]} <frame_file> <norad_id>\n'
+            f'Frame file can be KISS, SatNOGS data export or GetKISS+'
+        )
         exit(0)
     d = ImageDecode(argv[1], int(argv[2]))
