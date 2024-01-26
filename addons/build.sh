a26ef89a8bc94bbf53715cde259cc0f664c75ca3
Daniel Ekman <knegge@gmail.com>
2024-01-26 21:00:58 +0100

    Change SatDump build, add cmake parallel arg
    Signed-off-by: Daniel Ekman <knegge@gmail.com>

--- addons/build.sh
+++ addons/build.sh
@@ -8,9 +8,9 @@ else
     SATNOGS_IMAGE_TAG="master"
 fi
 REPO_ROOT="knegge"
-ARGS="
-    --build-arg SATNOGS_IMAGE_TAG=${SATNOGS_IMAGE_TAG} \
-"
+
+ARGS="  --build-arg SATNOGS_IMAGE_TAG=${SATNOGS_IMAGE_TAG}"
+#ARGS+=" --build-arg CMAKE_BUILD_PARALLEL_LEVEL=8"
 
 docker build \
     -t ${REPO_ROOT}/satnogs-client:${TAG} \
