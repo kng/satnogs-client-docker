c1a4a1eb74fc6e4390ee9ad109c47edaef679dae
Daniel Ekman <knegge@gmail.com>
2025-06-25 21:38:38 +0200

    Update Meteor demod&decode
    Signed-off-by: Daniel Ekman <knegge@gmail.com>

--- addons/scripts/meteor.sh
+++ addons/scripts/meteor.sh
@@ -12,7 +12,7 @@ BAUD="$6"    # $6 baudrate
 SCRIPT="$7"  # $7 script name, satnogs_bpsk.py
 
 # default values
-: "${METEOR_NORAD:=57166}"
+: "${METEOR_NORAD:=57166 59051}"
 : "${UDP_DUMP_PORT:=57356}"
 : "${SATNOGS_APP_PATH:=/tmp/.satnogs}"
 : "${SATNOGS_OUTPUT_PATH:=/tmp/.satnogs/data}"
@@ -24,7 +24,7 @@ SATNAME=$(echo "$TLE" | jq .tle0 | sed -e 's/ /_/g' | sed -e 's/[^A-Za-z0-9._-]/
 NORAD=$(echo "$TLE" | jq .tle2 | awk '{print $2}')
 
 if [ "${CMD^^}" == "START" ]; then
-  if [ -z ${METEOR_NORAD+x} ] || [[ ${METEOR_NORAD} =~ ${NORAD} ]]; then
+  if [ -z ${METEOR_NORAD+x} ] || [[ " ${METEOR_NORAD} " =~ .*\ ${NORAD}\ .* ]]; then
     echo "$PRG: $ID, Norad: $NORAD, Name: $SATNAME, Script: $SCRIPT"
     if [ -z "$UDP_DUMP_HOST" ]; then
       echo "Warning: UDP_DUMP_HOST not set, no data will be sent to the demod"
@@ -34,12 +34,15 @@ if [ "${CMD^^}" == "START" ]; then
       SAMP=144000
       echo "WARNING: find_samp_rate.py did not return valid sample rate!"
     fi
-    SYMRATE=$((${BAUD%.*} * 2))
-    if [ ${SYMRATE} == 80000 ]; then
-      INTERLACE="--int"
-    else
-      INTERLACE=""
-    fi
+    SYMRATE=72000
+    INTERLACE=""
+# The following is experimental, has only been used during commissioning
+#   SYMRATE=$((${BAUD%.*} * 2))
+#   if [ ${SYMRATE} == 80000 ]; then
+#     INTERLACE="--int"
+#   else
+#     INTERLACE=""
+#   fi
     ( udp2ishort.py -p "$UDP_DUMP_PORT" & echo $! > "$METEOR_PID" ) | \
     meteor_demod --batch --quiet -O 8 -f 128 -s "$SAMP" -r "$SYMRATE" -m oqpsk --bps 16 --stdout - | \
     meteor_decode --batch --quiet "$INTERLACE" --diff -a 65,65,64 -o "$IMAGE" - &
